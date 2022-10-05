3fd7d4438f7ff32876b15be72298dfcb21250705
vaaa <vaaa>
2022-10-05 14:46:22 +0200

    Initial commit

---
+++ searchbox/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Searchbox from './searchbox/Searchbox';
+
+function App() {
+  return (
+    <div className="App">
+     <Searchbox />
+    </div>
+  );
+}
+
+export default App;
